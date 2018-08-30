1b080829421e00ad3a9bd8a3f24409b229197628
John <cooks.johns@gmail.com>
2018-08-29 20:10:51 -0500

    All setUp with the example

--- src/com/company/java/Main.java
+++ src/com/company/java/Main.java
@@ -1,16 +1,26 @@
 package com.company.java;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
 public class Main {
 
+
     public static void main(String[] args) throws Exception {
-	// write your code here
-        System.out.println("Testing, Hello World");
+
+	something();
+
+
+
+    }
+
+    public static void something() {
 
         File input = new File("/Users/johnathoncook/IdeaProjects/CsvConverter/src/csv/Bodgeit.csv");
         File output = new File("/Users/johnathoncook/IdeaProjects/CsvConverter/src/csv/output/build.json");
@@ -19,14 +29,29 @@ public class Main {
         CsvMapper csvMapper = new CsvMapper();
 
         // Read data from CSV file
-        List<Object> readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+        List<Object> readAll = null;
+        try {
+            readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         ObjectMapper mapper = new ObjectMapper();
 
         // Write JSON formated data to output.json file
-        mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+        try {
+            mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Write JSON formated data to stdout
-        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+        try {
+            System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
     }
+
 }
